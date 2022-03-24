6327981b5d274d73db4f6166f05835a62c663cb6
Phạm Hồ Hữu Trí <oopscreeper2006@gmail.com>
2022-03-24 13:06:16 +0700

    idk

---
+++ math.js
@@ -0,0 +1,9 @@
+export const isSmaller = (a, b) => {
+    if (a > b) return b;
+        else
+    return a;
+}
+
+const PI = 3.14
+
+export default PI;
---
+++ script.js
@@ -0,0 +1,70 @@
+// function
+
+// function logger(name){
+//     let result = 'Hello ' + name;
+//     return result;
+// }
+
+// call function
+
+// console.log(logger("Nhan"));
+// let a = logger("Trí");
+// console.log(a);
+// logger("Jonny");
+
+//arrow function
+// const logger = (name) => {
+//     return ("Hello " + name);
+// }
+
+// let result = logger("Trí");
+// console.log(result);
+
+//Object
+// key: value
+// let person = {
+//     firstName: "Quang",
+//     lastName: "Tran",   
+//     "middle name": "Minh"
+// }; 
+
+// let person2 = {
+//     firstName: "Cek"
+// }
+//empty object
+
+// console.log(person["middle name"]);
+// console.log(person.firstName);
+
+// let firstname = person.firstname
+// console.log(Fn);
+
+// Destructuring
+
+// const {
+//     firstName: newName, lastName, age
+// } = person;
+
+// console.log(newName, lastName, age)
+
+// let fulName = "Huu Tri";
+// let age = 15;
+
+// let person = {fulName, age}
+// console.log(person)
+
+// Array
+// let numbers = [1, 2, 3, 4];
+
+
+// // index
+// let strings = ["a", "b", "c", "d"];
+
+// console.log(numbers[numbers.length - 1])
+
+// Modules: Import / export / export default
+
+// import PI, {isSmaller as compare} from "./math.js";
+
+// console.log(compare(4,3))
+// console.log(PI);
